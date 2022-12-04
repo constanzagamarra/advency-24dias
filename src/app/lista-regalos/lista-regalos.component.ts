c7b9f66828faae8b5ab73c580a06db7c4e234d3b
Constanza <cxnstanzagamarra@gmail.com>
2022-12-04 20:34:06 -0300

    dia 1 y dia 2

---
+++ src/app/lista-regalos/lista-regalos.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-regalos',
+  templateUrl: './lista-regalos.component.html',
+  styleUrls: ['./lista-regalos.component.css']
+})
+export class ListaRegalosComponent implements OnInit {
+  // regalos = {
+
+  //   regalo: {
+  //      nombre:'Auriculares'
+  
+  //       },
+  //   regalo1: {
+  //         nombre:'Dolares'
+  
+  //       },
+  //   regalo2: {
+  //         nombre:'Cursos'
+  //       },
+  //   regalo3: {
+  //         nombre:'Planchita'
+  
+  //       }
+  //     }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
